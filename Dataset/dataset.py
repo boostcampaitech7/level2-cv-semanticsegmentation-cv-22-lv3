6423625b55b615fb205a9d076b187f41162fa990
SeungMinChung <taky0315@naver.com>
2024-11-14 01:14:43 +0900

    [FEAT] Dataset.py 구현완료
    - customDataset을 구축하였습니다. config 파일로 관리가능하도록 구현하여 config 내에서 이미지 경로와 증강 기법을 활용하여 이에 맞는 이미지, 라벨을 반환하도록 설계하였습니다.
    Fixes : #12

---
+++ Dataset/dataset.py
@@ -0,0 +1,230 @@
+import yaml
+import os
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+from sklearn.model_selection import GroupKFold
+import cv2
+import json
+import torch
+import albumentations as A
+import albumentations as A
+import argparse
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+def load_config(config_path : str) -> dict:
+    with open(config_path, 'r') as data_config :
+        data_config = yaml.safe_load(data_config)
+    return data_config
+
+
+def check_image_label_pair(config) -> tuple[list[str], list[str]]:
+    train_data_path = config['data']['train_data_path']
+    train_label_path = config['data']['train_label_path']
+
+    # config에서 경로를 불러와 .png 파일들을 저장한다
+    images = {
+        os.path.relpath(os.path.join(root, fname), start = train_data_path)
+        for root, _, files in os.walk(train_data_path)
+        for fname in files
+        if os.path.splitext(fname)[1].lower() == '.png'
+    }
+
+    # config에서 셩로를 불러와 .json 파일들을 저장한다
+    labels = {
+        os.path.relpath(os.path.join(root, fname), start = train_label_path)
+        for root, _, files in os.walk(train_label_path)
+        for fname in files
+        if os.path.splitext(fname)[1].lower() == '.json'
+    }
+
+    # 두 파일의 확장자를 제거하여 파일 이름을 저장한다
+    jsons_fn_prefix = {os.path.splitext(fname)[0] for fname in labels}
+    pngs_fn_prefix = {os.path.splitext(fname)[0] for fname in images}
+
+    # 두 파일이름의 차이가 존재한다면 서로 매칭되지 않는 라벨과 이미지이므로 , 에러를 발생시킨다.
+    assert len(jsons_fn_prefix - pngs_fn_prefix) == 0
+    assert len(pngs_fn_prefix - jsons_fn_prefix) == 0
+
+    images = sorted(images)
+    labels = sorted(labels)
+
+    return images, labels
+
+
+def get_transforms(augmentations_config : dict):
+    transforms_list = []
+    for aug in augmentations_config:
+        if isinstance(aug, dict):
+            for aug_name, aug_params in aug.items():
+                try:
+                    # Albumentations에서 해당 증강 클래스 가져오기
+                    aug_class = getattr(A, aug_name)
+                    if aug_params is None:
+                        transforms_list.append(aug_class())
+                    else:
+                        transforms_list.append(aug_class(**aug_params))
+                except AttributeError:
+                    print(f"Albumentations에 '{aug_name}' 증강이 존재하지 않습니다.")
+        else:
+            # 파라미터 없이 증강 이름만 지정한 경우
+            try:
+                aug_class = getattr(A, aug)
+                transforms_list.append(aug_class())
+            except AttributeError:
+                print(f"Albumentations에 '{aug}' 증강이 존재하지 않습니다.")
+    return A.Compose(transforms_list)
+
+
+
+class XRayDataset(Dataset):
+    def __init__ (self, is_train = True, transforms=None, config=None):
+        images, labels = check_image_label_pair(config)
+        _imagenames = np.array(images)
+        _labelnames = np.array(labels)
+
+
+        # 한 사람의 왼손, 오른손을 하나의 그룹으로 묶어준다.
+        left_right_group = [os.path.dirname(fname) for fname in _imagenames]
+        # groupfold를 위한 더미 데이터 ( 파라미터를 맞춰주기 위함 )
+        dummy_for_groupfold = [0 for _ in _imagenames]
+
+        groupKFold = GroupKFold(n_splits = 5)
+
+        imagenames = []
+        labelnames = []
+
+        for i, (x,y) in enumerate(groupKFold.split(_imagenames, dummy_for_groupfold, left_right_group)):
+            if is_train:
+                if i == 0 :
+                    continue
+                else:
+                    imagenames += list(_imagenames[y])
+                    labelnames += list(_labelnames[y])
+
+            else:
+                imagenames += list(_imagenames[y])
+                labelnames += list(_labelnames[y])
+
+        self.imagenames = imagenames
+        self.labelnames = labelnames
+        self.is_train = is_train
+        self.transforms = transforms
+        self.config = config
+
+    def __len__(self) -> int:
+        return len(self.imagenames)
+    
+
+    def __getitem__(self, item : int) -> tuple[list[float], list[float]]:
+        image_name = self.imagenames[item]
+        image_path = os.path.join(self.config['data']['train_data_path'], image_name)
+
+        image = cv2.imread(image_path)
+        # image = image / 255 
+
+        label_name = self.labelnames[item]
+        label_path = os.path.join(self.config['data']['train_label_path'], label_name)
+        
+        # 라벨의 형태를 생성한다 : 이미지의 높이와 넓이 + 클래스 수 의 형태로 만든들고, 모두 0으로 만들어준다 ( H, W, Class)
+        label_shape = tuple(image.shape[:2]) + (len(self.config['data']['class']),)        
+        label = np.zeros(label_shape, dtype = np.uint8)
+
+        # label의 annotation 정보를 가지고 온다.
+        with open(label_path , 'r') as f:
+            annotations = json.load(f)
+        annotations = annotations['annotations']
+        
+        CLASS2IND = {v: i for i , v in enumerate(self.config['data']['class'])}
+        IND2CLASS = {v: k for k , v in CLASS2IND.items()}
+
+        for ann in annotations:
+            c = ann['label']
+            class_id = CLASS2IND[c]
+            points = np.array(ann['points'])
+
+            # polygone type을 mask type으로 변경하는 코드를 작성합니다.
+            class_label = np.zeros(image.shape[:2], dtype = np.uint8)
+            cv2.fillPoly(class_label, [points], 1)
+            label[..., class_id] = class_label
+
+        
+        if self.transforms is not None:
+            if self.is_train:
+                inputs = {'image': image, 'mask': label}
+                result = self.transforms(**inputs)
+                image = result['image']
+                label = result['mask']
+            else:
+                inputs = {'image': image}
+                result = self.transforms(**inputs)
+                image = result['image']
+
+    
+        image = image.transpose(2, 0, 1)
+        label = label.transpose(2, 0, 1)
+
+        image = torch.from_numpy(image).float()
+        label = torch.from_numpy(label).float()
+
+        return image, label
+
+
+
+
+if __name__ == "__main__":
+    # argparse를 사용하여 명령줄 인자 파싱
+    parser = argparse.ArgumentParser(description="Train Semantic Segmentation Model")
+    parser.add_argument('--config', type=str, default='configs/data_config.yaml', help='Path to the config file')
+    args = parser.parse_args()
+
+    # 설정 파일 불러오기
+    config = load_config(args.config)
+
+    # 증강 설정 불러오기
+    train_transforms_config = config.get('augmentation', {}).get('train', [])
+    valid_transforms_config = config.get('augmentation', {}).get('valid', [])
+
+    # 증강 파이프라인 생성
+    train_transforms = get_transforms(train_transforms_config)
+    valid_transforms = get_transforms(valid_transforms_config)
+
+    # 데이터셋 생성
+    train_dataset = XRayDataset(is_train=True, transforms=train_transforms, config=config)
+    valid_dataset = XRayDataset(is_train=False, transforms=valid_transforms, config=config)
+
+    # 데이터셋 사용 예시
+    image, label = train_dataset[0]
+    print("Image shape:", image.shape)
+    print("Label shape:", label.shape)
+    print("Train dataset length:", len(train_dataset))
+
+
+    # 시각화를 위한 팔레트를 설정합니다.
+    PALETTE = [
+        (220, 20, 60), (119, 11, 32), (0, 0, 142), (0, 0, 230), (106, 0, 228),
+        (0, 60, 100), (0, 80, 100), (0, 0, 70), (0, 0, 192), (250, 170, 30),
+        (100, 170, 30), (220, 220, 0), (175, 116, 175), (250, 0, 30), (165, 42, 42),
+        (255, 77, 255), (0, 226, 252), (182, 182, 255), (0, 82, 0), (120, 166, 157),
+        (110, 76, 0), (174, 57, 255), (199, 100, 0), (72, 0, 118), (255, 179, 240),
+        (0, 125, 92), (209, 0, 151), (188, 208, 182), (0, 220, 176),
+    ]
+
+    # 시각화 함수입니다. 클래스가 2개 이상인 픽셀을 고려하지는 않습니다.
+    def label2rgb(label):
+        image_size = label.shape[1:] + (3, )
+        image = np.zeros(image_size, dtype=np.uint8)
+        
+        for i, class_label in enumerate(label):
+            image[class_label == 1] = PALETTE[i]
+            
+        return image
+
+    fig, ax = plt.subplots(1, 2, figsize=(24, 12))
+    ax[0].imshow(image[0])    # color map 적용을 위해 channel 차원을 생략합니다.
+    ax[1].imshow(label2rgb(label))
+
+    plt.show()
+
+    
