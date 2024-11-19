542a1a6aa3f900769d4eae60ffa2584b8ec2c84a
bigstar <2002bigstar@gmail.com>
2024-11-19 01:22:33 +0000

    [FIX] validation 수정 및 wandb 시각화 비활성화
    - wandb 시각화에서 에러나는 부분 임시방편으로 주석처리
    Related to: #45

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,14 +1,23 @@
 import torch
-import torch.nn.functional as F
+# import torch.nn.functional as F
 
 
-def dice_coef(preds, targets, num_classes, epsilon=1e-7):
-    preds_one_hot = F.one_hot(preds, num_classes=num_classes).permute(0, 3, 1, 2).float()  
-    targets_one_hot = F.one_hot(targets, num_classes=num_classes).permute(0, 3, 1, 2).float()  
+# def dice_coef(preds, targets, num_classes, epsilon=1e-7):
+#     preds_one_hot = F.one_hot(preds, num_classes=num_classes).permute(0, 3, 1, 2).float()  
+#     targets_one_hot = F.one_hot(targets, num_classes=num_classes).permute(0, 3, 1, 2).float()  
 
-    dims = (0, 2, 3)  
-    intersection = torch.sum(preds_one_hot * targets_one_hot, dims)
-    cardinality = torch.sum(preds_one_hot + targets_one_hot, dims)
-    dice = (2. * intersection + epsilon) / (cardinality + epsilon)
+#     dims = (0, 2, 3)  
+#     intersection = torch.sum(preds_one_hot * targets_one_hot, dims)
+#     cardinality = torch.sum(preds_one_hot + targets_one_hot, dims)
+#     dice = (2. * intersection + epsilon) / (cardinality + epsilon)
 
-    return dice  
+#     return dice  
+
+
+def dice_coef(y_true, y_pred):
+    y_true_f = y_true.flatten(2)
+    y_pred_f = y_pred.flatten(2)
+    intersection = torch.sum(y_true_f * y_pred_f, -1)
+
+    eps = 0.0001
+    return (2. * intersection + eps) / (torch.sum(y_true_f, -1) + torch.sum(y_pred_f, -1) + eps)
--- src/validation/validation.py
+++ src/validation/validation.py
@@ -1,7 +1,9 @@
+import time
 import torch
 import random
 import wandb
 import numpy as np
+from datetime import timedelta
 from tqdm.auto import tqdm
 import torch.nn.functional as F
 from utils.metrics import dice_coef
@@ -24,21 +26,25 @@ def validation(epoch, model, data_loader, criterion, config=None):
     preds_to_visualize = []
     masks_to_visualize = []
 
+    val_start = time.time()
     with torch.no_grad():
         n_class = len(config.data.classes)
         total_loss = 0
         cnt = 0
 
         for _, (images, masks) in tqdm(enumerate(data_loader), total=len(data_loader)):
+            # 이미지를 GPU로 옮김
             images, masks = images.cuda(), masks.cuda()
-            
-            
+
+
             outputs = get_model_output(model, config.model.library, images)
-            
+
+            # 출력 텐서의 높이와 너비 저장
             output_h, output_w = outputs.size(-2), outputs.size(-1)
+            # 실제 마스크 텐서의 높이와 너비 저장
             mask_h, mask_w = masks.size(-2), masks.size(-1)
             
-          
+            # 출력과 마스크 크기가 다를 경우 출력 크기를 마스크 크기로 보간하여 조정
             if output_h != mask_h or output_w != mask_w:
                 outputs = F.interpolate(outputs, size=(mask_h, mask_w), mode="bilinear")
             
@@ -47,13 +53,22 @@ def validation(epoch, model, data_loader, criterion, config=None):
             total_loss += loss.item()
             cnt += 1
             
-            outputs = torch.softmax(outputs, dim=1)
-            preds = outputs.argmax(dim=1).detach().cpu() 
-            gt = masks.argmax(dim=1).detach().cpu()      
+            # softmax는 각 클래스 확률의 합이 1이 되도록 하므로 적절하지 않음
+            # outputs = torch.softmax(outputs, dim=1)
+
+            # 출력에 sigmoid 활성화 함수 적용하여 확률 값으로 변환
+            outputs = torch.sigmoid(outputs)            
+            # 임계값 기준으로 이진화하여 0 또는 1로 변환
+            outputs = (outputs > config.data.valid.threshold).float()
+                        
+            preds = outputs.detach().cpu()
+            gt = masks.detach().cpu()
             
-         
-            dice = dice_coef(gt, preds, num_classes=n_class)  
-            dices.append(dice)
+            # metrics.py에서 dice_coef 함수 수정
+            # dice = dice_coef(gt, preds, num_classes=n_class)  
+            # CPU로 넘기지 말고 그대로 GPU에서 진행   
+            dice = dice_coef(outputs, masks)
+            dices.append(dice.detach().cpu())
                 
    
             if len(images_to_visualize) < 5:
@@ -64,13 +79,13 @@ def validation(epoch, model, data_loader, criterion, config=None):
                 preds_to_visualize.extend(preds[:num_to_take].cpu())
                 masks_to_visualize.extend(gt[:num_to_take].cpu())
 
-
+    val_end = time.time() - val_start
     avg_loss = total_loss / cnt
-    dices = torch.stack(dices, dim=0)  
+    dices = torch.cat(dices, dim=0) 
     dices_per_class = dices.mean(dim=0)  
     avg_dice = dices_per_class.mean().item()
 
-
+    print("Elapsed time: {}\n".format(timedelta(seconds=val_end)))
     print(f'Epoch [{epoch}], Validation Loss: {avg_loss:.4f}, Average Dice Coefficient: {avg_dice:.4f}')
     for idx, cls in enumerate(config.data.classes):
         print(f"{cls:<12}: {dices_per_class[idx].item():.4f}")
@@ -88,11 +103,11 @@ def validation(epoch, model, data_loader, criterion, config=None):
     wandb.log(dice_dict)
 
   
-    if len(images_to_visualize) > 0:
-        figures = visualize_predictions(images_to_visualize, preds_to_visualize, masks_to_visualize, max_visualize=5)
+    # if len(images_to_visualize) > 0:
+    #     figures = visualize_predictions(images_to_visualize, preds_to_visualize, masks_to_visualize, max_visualize=5)
 
 
-        if figures:
-            wandb.log({"validation_results": figures, "epoch": epoch})
+    #     if figures:
+    #         wandb.log({"validation_results": figures, "epoch": epoch})
 
     return avg_dice
