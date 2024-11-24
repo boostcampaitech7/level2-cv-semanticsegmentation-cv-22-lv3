c21b959c7f7aeaaa9ee1a850d0d983e7a9e9bcd2
seungminchung <taky0315@naver.com>
2024-11-24 13:35:56 +0000

    [FIX] 단순 주석 코드를 제거

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,18 +1,4 @@
 import torch
-# import torch.nn.functional as F
-
-
-# def dice_coef(preds, targets, num_classes, epsilon=1e-7):
-#     preds_one_hot = F.one_hot(preds, num_classes=num_classes).permute(0, 3, 1, 2).float()  
-#     targets_one_hot = F.one_hot(targets, num_classes=num_classes).permute(0, 3, 1, 2).float()  
-
-#     dims = (0, 2, 3)  
-#     intersection = torch.sum(preds_one_hot * targets_one_hot, dims)
-#     cardinality = torch.sum(preds_one_hot + targets_one_hot, dims)
-#     dice = (2. * intersection + epsilon) / (cardinality + epsilon)
-
-#     return dice  
-
 
 def dice_coef(y_true, y_pred):
     y_true_f = y_true.flatten(2)
